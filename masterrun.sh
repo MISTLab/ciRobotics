d68d10a5c63d05a3c17e855346d6b839b665a2c2
u16 <admin@example.com>
2018-08-13 18:05:09 +0200

    runner auto set done

--- masterrun.sh
+++ masterrun.sh
@@ -5,7 +5,7 @@ mkdir  services
 cp ./docker-compose.yml ./services/docker-compose.yml
 #cp ./runservice.sh ./services/runservice.sh
 
-sed -n "s/localhost/$(hostname)/p" ./services/docker-compose.yml
+sed -i "s/localhost/$(hostname)/" ./services/docker-compose.yml
 cd services
 docker-compose up -d 
 cd ..
