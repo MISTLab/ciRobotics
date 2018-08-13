8e98e1d0c411608f6e38882f1cb0d71ef622e4b6
u16 <admin@example.com>
2018-08-13 09:27:39 +0200

    add .sh

---
+++ masterrun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo $(hostname)
+mkdir  services
+cp ./docker-compose.yml ./services/docker-compose.yml
+cp ./runservice.sh ./services/runservice.sh
+
+sed -n "s/localhost/$(hostname)/p" ./services/docker-compose.yml
+bash ./services/runservice.sh
