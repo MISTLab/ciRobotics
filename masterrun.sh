ea9337afa0a8fd0f534722fdfb24eeeaf562dd42
u16 <admin@example.com>
2018-08-14 12:40:34 +0200

    Add fornodes for other nodes to use

--- masterrun.sh
+++ masterrun.sh
@@ -5,8 +5,23 @@ mkdir  services
 cp ./docker-compose.yml ./services/docker-compose.yml
 #cp ./runservice.sh ./services/runservice.sh
 
+
+destdir=./fornodes.txt
+echo $(hostname)>"$destdir"
+IPADDR="$(ip route get 1 | awk '{print $NF;exit}')"
+echo $IPADDR>>"$destdir"
+docker swarm leave -f
+SWARMJOIN="$(docker swarm init --advertise-addr $IPADDR |awk '/SWMTKN/{print}')"
+echo $SWARMJOIN>>"$destdir"
+echo $SWARMJOIN
+
+
 sed -i "s/localhost/$(hostname)/" ./services/docker-compose.yml
 cd services
 docker-compose up -d 
 cd ..
 
+
+git add "$destdir"
+git commit -a -m "Add fornodes for other nodes to use"
+git push
