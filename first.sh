8b870f8a7ed16620769bb9f5506eb7a5743669cb
u16 <admin@example.com>
2018-08-14 12:29:07 +0200

    Add fornodes for other nodes to use

--- first.sh
+++ first.sh
@@ -8,10 +8,10 @@ SWARMJOIN="$(docker swarm init --advertise-addr $IPADDR |awk '/SWMTKN/{print}')"
 echo $SWARMJOIN>>"$destdir"
 echo $SWARMJOIN
 
-echo "Please type your project name, which is used as service name"
-read PROJECTNAME
-echo $PROJECTNAME
-docker service create --mode=global  --with-registry-auth --constraint node.role==worker -d --name $PROJECTNAME  --env="DISPLAY" --env QT_X11_NO_MITSHM=1   --mount type=bind,source=/tmp/.X11-unix,destination=/tmp/.X11-unix u16-vbw2:4567/root/$PROJECTNAME ping google.com
+#echo "Please type your project name, which is used as service name"
+#read PROJECTNAME
+#echo $PROJECTNAME
+#docker service create --mode=global  --with-registry-auth --constraint node.role==worker -d --name $PROJECTNAME  --env="DISPLAY" --env QT_X11_NO_MITSHM=1   --mount type=bind,source=/tmp/.X11-unix,destination=/tmp/.X11-unix $(hostname):4567/root/$PROJECTNAME ping google.com
 
 git add "$destdir"
 git commit -a -m "Add fornodes for other nodes to use"
